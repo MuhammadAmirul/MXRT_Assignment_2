1dcfc059bd8a01fd1a941c7979afced935618908
Amirul <amirulsafiee@yahoo.com.sg>
2021-05-29 17:06:58 +0800

    Added VirusSpawner.cs Script
    Changed Unity player settings.

--- P02_1902032J_Amirul_MXRT_Assignment_2/Assets/UI/Scripts/VirusSpawner.cs
+++ P02_1902032J_Amirul_MXRT_Assignment_2/Assets/UI/Scripts/VirusSpawner.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class VirusSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private ARPlaneManager arPlaneManager;
+
+    [SerializeField]
+    private GameObject virusGameObject;
+
+    private int virusSpawn = 0;
+    private int maxVirusSpawn = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        virusSpawn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(TimelyVirusSpawn());
+    }
+
+    IEnumerator TimelyVirusSpawn()
+    {
+        yield return new WaitForSeconds(2f);
+
+        arPlaneManager.planesChanged += SpawnVirus;
+    }
+
+    void SpawnVirus(ARPlanesChangedEventArgs virusObject)
+    {
+        if (virusSpawn < maxVirusSpawn)
+        {
+            foreach (var plane in virusObject.added)
+            {
+                Instantiate(virusGameObject, plane.center, Quaternion.identity);
+                virusSpawn++;
+            }
+        }
     }
 }
